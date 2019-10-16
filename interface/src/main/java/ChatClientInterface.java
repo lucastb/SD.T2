bb608e162354a8d200a6c1030c40bf4d240cbddc
Lucca_Santos <lucca_santos@dell.com>
2019-10-15 21:58:40 -0300

    Adicionando a segunda interface que irá realizar o envio de mensagens.

---
+++ interface/src/main/java/ChatClientInterface.java
@@ -0,0 +1,9 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface ChatClientInterface extends Remote {
+
+	public int message(String message) throws RemoteException;
+
+	public int kick() throws RemoteException;
+}
